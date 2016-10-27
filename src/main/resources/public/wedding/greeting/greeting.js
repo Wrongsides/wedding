3b3790c7669f55e26ba49a7f4a636caa483e687a
john.byrne <john.byrne@autotrader.co.uk>
2016-10-27 10:59:13 +0100

    In-lined the controller, removed redundant binding and initialiser

--- src/main/resources/public/wedding/greeting/greeting.js
+++ src/main/resources/public/wedding/greeting/greeting.js
@@ -1,16 +1,10 @@
-function GreetingController($http){
-    var ctrl = this;
-    ctrl.greeting = {};
-    $http.get("/api/greeting").success(function(response) {
-        ctrl.greeting = response;
-    });
-}
-
 angular.module('weddingApp')
     .component('greeting', {
         templateUrl: 'wedding/greeting/greeting.html',
-        controller: GreetingController,
-        bindings: {
-            greeting: '='
-        }
+        controller: function($http){
+                        var ctrl = this;
+                        $http.get("/api/greeting").success(function(response) {
+                            ctrl.greeting = response;
+                        });
+                    }
     });
